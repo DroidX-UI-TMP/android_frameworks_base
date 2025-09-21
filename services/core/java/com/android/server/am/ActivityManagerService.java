Kalpu-24/frameworks_base

[Boost-framework] initial system integration
Change-Id: I3b56ba3a058aecfd2d1e9d136c3528f79af34b99
Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

services/core/java/com/android/server/am/ActivityManagerService.java
@@ -314,6 +314,7 @@ import android.database.ContentObserver;
 import android.graphics.Rect;
 import android.hardware.display.DisplayManager;
 import android.hardware.display.DisplayManagerInternal;
+import android.hardware.power.Mode;
 import android.net.Uri;
 import android.os.AppZygote;
 import android.os.BatteryStats;
@@ -493,10 +494,13 @@ import dalvik.system.VMRuntime;
 
 import libcore.util.EmptyArray;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileDescriptor;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
@@ -1326,6 +1330,9 @@ public class ActivityManagerService extends IActivityManager.Stub
 
     @GuardedBy("this") boolean mCallFinishBooting = false;
     @GuardedBy("this") boolean mBootAnimationComplete = false;
+
+    boolean mBoostingAnimation = false;
+    int mTopAppPid = -1;
 
     final Context mContext;
 
@@ -5255,6 +5262,7 @@ public class ActivityManagerService extends IActivityManager.Stub
 
             // Start PSI monitoring in LMKD if it was skipped earlier.
             ProcessList.startPsiMonitoringAfterBoot();
+            initTaskProfiles();
 
             mUserController.onBootComplete(
                     new IIntentReceiver.Stub() {
@@ -8281,8 +8289,20 @@ public class ActivityManagerService extends IActivityManager.Stub
      * @return {@code true} if this succeeded.
      */
     public static boolean scheduleAsFifoPriority(int tid, boolean suppressLogs) {
+        return scheduleAsFifoPriority(tid, suppressLogs, 1);
+    }
+
+    /**
+     * Schedule the given thread an FIFO scheduling priority.
+     *
+     * @param tid the tid of the thread to adjust the scheduling of.
+     * @param suppressLogs {@code true} if any error logging should be disabled.
+     *
+     * @return {@code true} if this succeeded.
+     */
+    public static boolean scheduleAsFifoPriority(int tid, boolean suppressLogs, int prio) {
         try {
-            Process.setThreadScheduler(tid, Process.SCHED_FIFO | Process.SCHED_RESET_ON_FORK, 1);
+            Process.setThreadScheduler(tid, Process.SCHED_FIFO | Process.SCHED_RESET_ON_FORK, prio);
             return true;
         } catch (IllegalArgumentException e) {
             if (!suppressLogs) {
@@ -8296,18 +8316,23 @@ public class ActivityManagerService extends IActivityManager.Stub
         return false;
     }
 
+    @GuardedBy("mProcLock")
+    static void setFifoPriority(@NonNull ProcessRecord app, boolean enable) {
+        setFifoPriority(app, enable, 1);
+    }
+
     /**
      * Switches the priority between SCHED_FIFO and SCHED_OTHER for the main thread and render
      * thread of the given process.
      */
     @GuardedBy("mProcLock")
-    static void setFifoPriority(@NonNull ProcessRecord app, boolean enable) {
+    static void setFifoPriority(@NonNull ProcessRecord app, boolean enable, int prio) {
         final int pid = app.getPid();
         final int renderThreadTid = app.getRenderThreadTid();
         if (enable) {
-            scheduleAsFifoPriority(pid, true /* suppressLogs */);
+            scheduleAsFifoPriority(pid, true /* suppressLogs */, prio);
             if (renderThreadTid != 0) {
-                scheduleAsFifoPriority(renderThreadTid, true /* suppressLogs */);
+                scheduleAsFifoPriority(renderThreadTid, true /* suppressLogs */, prio);
             }
         } else {
             scheduleAsRegularPriority(pid, true /* suppressLogs */);
@@ -19521,5 +19546,288 @@ public class ActivityManagerService extends IActivityManager.Stub
             }
         }
         return token;
+    }
+    
+    public ProcessRecord getProcessRecord(String str) {
+        ProcessRecord processRecordLocked = null;
+        synchronized (mProcLock) {
+            try {
+                int currentUserId = getCurrentUserId();
+                int packageUid = getPackageManagerInternal().getPackageUid(str, 0, currentUserId);
+                processRecordLocked = getProcessRecordLocked(str, packageUid);
+            } catch (Exception e) {
+            }
+        }
+        return processRecordLocked;
+    }
+
+    @Override
+    public void releaseMemory(int minAdj, int maxKillCount, boolean includeUIProcesses, boolean skipCamera) {
+        if (minAdj == 0) return;
+
+        try {
+            ArrayList<ProcessRecord> processList = 
+                (ArrayList<ProcessRecord>) mProcessList.getLruProcessesLOSP().clone();
+
+            ArrayList<ProcessToKill> toKill = new ArrayList<>();
+
+            for (ProcessRecord record : processList) {
+                if (record != null && record.getSetAdj() >= minAdj) {
+                    boolean hasUI = record.hasActivities();
+                    if (!hasUI || includeUIProcesses) {
+                        toKill.add(new ProcessToKill(record));
+                    }
+                }
+            }
+
+            Collections.sort(toKill, new ProcessComparator());
+
+            int killedCount = 0;
+            for (ProcessToKill info : toKill) {
+                Process.killProcess(info.pid);
+                killedCount++;
+                if (killedCount >= maxKillCount) return;
+            }
+
+        } catch (Exception e) {
+        }
+    }
+
+    @Override
+    public boolean isSwipeToScreenshotGestureActive() {
+        synchronized (this) {
+            return mIsSwipeToScrenshotEnabled && SystemProperties.getBoolean("sys.android.screenshot", false);
+        }
+    }
+   
+    @Override
+    public void executeAdjustCpusetCpus(String path, String cpuset) {
+        File file = new File(path);
+        if (!file.exists()) {
+            return;
+        }
+        try (FileWriter writer = new FileWriter(file)) {
+            writer.write(cpuset);
+            writer.flush();
+        } catch (IOException e) {
+            Log.e("executeAdjustCpusetCpus", "Failed to write to " + path + ": " + e.getMessage());
+        }
+    }
+
+    @Override
+    public void adjustCpusetCpus(String path, String cpuset, long durationMillis) {
+        File file = new File(path);
+        if (!file.exists()) {
+            return;
+        }
+        String originalCpuset = null;
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            originalCpuset = reader.readLine();
+        } catch (IOException e) {
+            Log.e("adjustCpusetCpus", "Failed to read original cpuset from " + path + ": " + e.getMessage());
+            return;
+        }
+
+        executeAdjustCpusetCpus(path, cpuset);
+
+        String restoreCpuset = originalCpuset;
+        mHandler.postDelayed(() -> executeAdjustCpusetCpus(path, restoreCpuset), durationMillis);
+    }
+
+    private String getTopAppPackageName() {
+        String currentPackage;
+        try {
+            RunningTaskInfo rti = mActivityTaskManager.getTasks(
+                1, false /* filterVisibleRecents */, false /*keepIntentExtra */,
+                INVALID_DISPLAY).get(0);
+            currentPackage = rti.topActivity.getPackageName();
+        } catch (Exception e) {
+            currentPackage = null;
+        }
+        return currentPackage;
+    }
+
+    private boolean isTopAppGame(String packageName) {
+        if (packageName == null) return false;
+        boolean isGame = false;
+        try {
+            ApplicationInfo ai = mContext.getPackageManager().getApplicationInfo(packageName, 0);
+            if(ai != null) {
+                isGame = (ai.category == ApplicationInfo.CATEGORY_GAME) ||
+                        ((ai.flags & ApplicationInfo.FLAG_IS_GAME) ==
+                            ApplicationInfo.FLAG_IS_GAME);
+            }
+        } catch (Exception e) {
+            return false;
+        }
+        return isGame;
+    }
+
+    @Override
+    public void animationBoost(int pid, boolean enabled) {
+        ProcessRecord curProc;
+        synchronized (mPidsSelfLocked) {
+            curProc = mPidsSelfLocked.get(pid);
+        }
+        if (curProc == null) {
+            return;
+        }
+
+        if (enabled) {
+            String topApp = getTopAppPackageName();
+            if (topApp == null) {
+                return;
+            }
+
+            ProcessRecord topAppProc = getProcessRecord(topApp);
+            if (topAppProc == null) {
+                return;
+            }
+
+            int topAppPid = topAppProc.getPid();
+            final boolean isGame = isTopAppGame(topApp);
+            int group = isGame
+                    ? Process.THREAD_GROUP_DEFAULT 
+                    : Process.THREAD_GROUP_BACKGROUND;
+            int priority = isGame 
+                    ? Process.THREAD_PRIORITY_DEFAULT 
+                    : Process.THREAD_PRIORITY_BACKGROUND;
+            try {
+                Process.setProcessGroup(topAppPid, group);
+                Process.setThreadGroupAndCpuset(topAppPid, group);
+                Process.setThreadPriority(topAppPid, priority);
+                mTopAppPid = topAppPid;
+            } catch (Exception e) {
+                Slog.w(TAG, "Failed to demote top-app process: " + e);
+                mTopAppPid = -1;
+                return;
+            }
+        } else {
+            if (mTopAppPid != -1) {
+                try {
+                    Process.setProcessGroup(mTopAppPid, Process.THREAD_GROUP_TOP_APP);
+                    Process.setThreadGroupAndCpuset(mTopAppPid, Process.THREAD_GROUP_TOP_APP);
+                    Process.setThreadPriority(mTopAppPid, Process.THREAD_PRIORITY_TOP_APP_BOOST);
+                } catch (Exception e) {
+                    Slog.w(TAG, "Failed to restore top-app process group: " + e);
+                } finally {
+                    mTopAppPid = -1;
+                }
+            }
+        }
+
+        setFifoPriority(curProc, enabled, 99);
+        mBoostingAnimation = enabled;
+    }
+
+    @Override
+    public void setThreadAffinity(int pid, int affinity) {
+        ProcessRecord curProc;
+        synchronized (mPidsSelfLocked) {
+            curProc = mPidsSelfLocked.get(pid);
+        }
+        if (curProc == null) {
+            return;
+        }
+
+        int threadGroup = (affinity == 0)
+                ? Process.THREAD_GROUP_TOP_APP
+                : Process.THREAD_GROUP_BACKGROUND;
+
+        Process.setThreadGroupAndCpuset(pid, threadGroup);
+        Process.setThreadAffinity(pid, affinity);
+        
+        int rTid = curProc.getRenderThreadTid();
+        if (rTid == 0) return;
+        Process.setThreadGroupAndCpuset(rTid, threadGroup);
+        Process.setThreadAffinity(rTid, affinity);
+    }
+
+    @Override
+    public boolean isBoostingAnimation() {
+        return mBoostingAnimation;
+    }
+
+    @Override
+    public void setPerformanceMode(boolean enabled) {
+        if (enabled && isTopAppGame(getTopAppPackageName())) return;
+        if (mLocalPowerManager != null) {
+            // alway reset to retrigger LAUNCH powerhint
+            if (enabled) {
+                mLocalPowerManager.setPowerMode(Mode.LAUNCH, false);
+            }
+            mLocalPowerManager.setPowerMode(Mode.LAUNCH, enabled);
+            mLocalPowerManager.setPowerMode(
+                PowerManagerInternal.MODE_FIXED_PERFORMANCE, enabled);
+        }
+    }
+
+    public class ProcessComparator implements Comparator<ProcessToKill> {
+        @Override
+        public int compare(ProcessToKill p1, ProcessToKill p2) {
+            return Integer.compare(p2.adj, p1.adj);
+        }
+    }
+
+    public static final class ProcessToKill {
+        public int adj;
+        public String name; 
+        public int pid;
+        public int uid;
+        public ProcessRecord record;
+
+        public ProcessToKill(ProcessRecord record) {
+            this.pid = record.getPid();
+            this.uid = record.uid;
+            this.adj = record.getSetAdj();
+            this.name = record.processName;
+            this.record = record;
+        }
+    }
+    
+    private void initTaskProfiles() {
+        String[] bgProfiles = { "ProcessCapacityLow" };
+        String[] bgProcs = { "kswapd", "kcompactd" };
+        setTaskProfilesForProcs(bgProcs, bgProfiles);
+    }
+    
+    public static void setTaskProfilesForProcs(String[] procGroups, String[] profiles) {
+        File procDir = new File("/proc");
+        File[] entries = procDir.listFiles(file -> file.isDirectory() && file.getName().matches("\\d+"));
+        if (entries == null) {
+            Slog.w("setTaskProfilesForProcs", "/proc not accessible or empty.");
+            return;
+        }
+
+        for (File pidDir : entries) {
+            File commFile = new File(pidDir, "comm");
+            String processName = null;
+
+            try (BufferedReader reader = new BufferedReader(new FileReader(commFile))) {
+                processName = reader.readLine();
+            } catch (IOException e) {
+                Slog.w("setTaskProfilesForProcs", "Could not read " + commFile.getPath() + ": " + e);
+                continue;
+            }
+
+            if (processName == null) continue;
+
+            for (String proc : procGroups) {
+                if (processName.contains(proc)) {
+                    try {
+                        int pid = Integer.parseInt(pidDir.getName());
+                        Process.setTaskProfiles(pid, profiles);
+                        Slog.i("setTaskProfilesForProcs", "Applied profiles " + Arrays.toString(profiles) +
+                                " to process " + processName + " (PID " + pid + ")");
+                    } catch (NumberFormatException e) {
+                        Slog.w("setTaskProfilesForProcs", "Invalid PID: " + pidDir.getName());
+                    } catch (Exception e) {
+                        Slog.w("setTaskProfilesForProcs", "Failed to set profiles for PID " + pidDir.getName() + ": " + e);
+                    }
+                    break;
+                }
+            }
+        }
     }
 }

DroidX-UI-TMP/android_frameworks_base

services/core/java/com/android/server/am/ActivityManagerService.java
@@ -314,6 +314,7 @@ import android.database.ContentObserver;
 import android.graphics.Rect;
 import android.hardware.display.DisplayManager;
 import android.hardware.display.DisplayManagerInternal;
+import android.hardware.power.Mode;
 import android.net.Uri;
 import android.os.AppZygote;
 import android.os.BatteryStats;
@@ -493,10 +494,13 @@ import dalvik.system.VMRuntime;
 
 import libcore.util.EmptyArray;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileDescriptor;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
@@ -1326,6 +1330,9 @@ public class ActivityManagerService extends IActivityManager.Stub
 
     @GuardedBy("this") boolean mCallFinishBooting = false;
     @GuardedBy("this") boolean mBootAnimationComplete = false;
+
+    boolean mBoostingAnimation = false;
+    int mTopAppPid = -1;
 
     final Context mContext;
 
@@ -5255,6 +5262,7 @@ public class ActivityManagerService extends IActivityManager.Stub
 
             // Start PSI monitoring in LMKD if it was skipped earlier.
             ProcessList.startPsiMonitoringAfterBoot();
+            initTaskProfiles();
 
             mUserController.onBootComplete(
                     new IIntentReceiver.Stub() {
@@ -8275,8 +8283,20 @@ public class ActivityManagerService extends IActivityManager.Stub
      * @return {@code true} if this succeeded.
      */
     public static boolean scheduleAsFifoPriority(int tid, boolean suppressLogs) {
+        return scheduleAsFifoPriority(tid, suppressLogs, 1);
+    }
+
+    /**
+     * Schedule the given thread an FIFO scheduling priority.
+     *
+     * @param tid the tid of the thread to adjust the scheduling of.
+     * @param suppressLogs {@code true} if any error logging should be disabled.
+     *
+     * @return {@code true} if this succeeded.
+     */
+    public static boolean scheduleAsFifoPriority(int tid, boolean suppressLogs, int prio) {
         try {
-            Process.setThreadScheduler(tid, Process.SCHED_FIFO | Process.SCHED_RESET_ON_FORK, 1);
+            Process.setThreadScheduler(tid, Process.SCHED_FIFO | Process.SCHED_RESET_ON_FORK, prio);
             return true;
         } catch (IllegalArgumentException e) {
             if (!suppressLogs) {
@@ -8290,18 +8310,23 @@ public class ActivityManagerService extends IActivityManager.Stub
         return false;
     }
 
+    @GuardedBy("mProcLock")
+    static void setFifoPriority(@NonNull ProcessRecord app, boolean enable) {
+        setFifoPriority(app, enable, 1);
+    }
+
     /**
      * Switches the priority between SCHED_FIFO and SCHED_OTHER for the main thread and render
      * thread of the given process.
      */
     @GuardedBy("mProcLock")
-    static void setFifoPriority(@NonNull ProcessRecord app, boolean enable) {
+    static void setFifoPriority(@NonNull ProcessRecord app, boolean enable, int prio) {
         final int pid = app.getPid();
         final int renderThreadTid = app.getRenderThreadTid();
         if (enable) {
-            scheduleAsFifoPriority(pid, true /* suppressLogs */);
+            scheduleAsFifoPriority(pid, true /* suppressLogs */, prio);
             if (renderThreadTid != 0) {
-                scheduleAsFifoPriority(renderThreadTid, true /* suppressLogs */);
+                scheduleAsFifoPriority(renderThreadTid, true /* suppressLogs */, prio);
             }
         } else {
             scheduleAsRegularPriority(pid, true /* suppressLogs */);
@@ -19515,5 +19540,278 @@ public class ActivityManagerService extends IActivityManager.Stub
             }
         }
         return token;
+    }
+    
+    public ProcessRecord getProcessRecord(String str) {
+        ProcessRecord processRecordLocked = null;
+        synchronized (mProcLock) {
+            try {
+                int currentUserId = getCurrentUserId();
+                int packageUid = getPackageManagerInternal().getPackageUid(str, 0, currentUserId);
+                processRecordLocked = getProcessRecordLocked(str, packageUid);
+            } catch (Exception e) {
+            }
+        }
+        return processRecordLocked;
+    }
+
+    @Override
+    public void releaseMemory(int minAdj, int maxKillCount, boolean includeUIProcesses, boolean skipCamera) {
+        if (minAdj == 0) return;
+
+        try {
+            ArrayList<ProcessRecord> processList = 
+                (ArrayList<ProcessRecord>) mProcessList.getLruProcessesLOSP().clone();
+
+            ArrayList<ProcessToKill> toKill = new ArrayList<>();
+
+            for (ProcessRecord record : processList) {
+                if (record != null && record.getSetAdj() >= minAdj) {
+                    boolean hasUI = record.hasActivities();
+                    if (!hasUI || includeUIProcesses) {
+                        toKill.add(new ProcessToKill(record));
+                    }
+                }
+            }
+
+            Collections.sort(toKill, new ProcessComparator());
+
+            int killedCount = 0;
+            for (ProcessToKill info : toKill) {
+                Process.killProcess(info.pid);
+                killedCount++;
+                if (killedCount >= maxKillCount) return;
+            }
+
+        } catch (Exception e) {
+        }
+    }
+
+    @Override
+    public void executeAdjustCpusetCpus(String path, String cpuset) {
+        File file = new File(path);
+        if (!file.exists()) {
+            return;
+        }
+        try (FileWriter writer = new FileWriter(file)) {
+            writer.write(cpuset);
+            writer.flush();
+        } catch (IOException e) {
+            Log.e("executeAdjustCpusetCpus", "Failed to write to " + path + ": " + e.getMessage());
+        }
+    }
+
+    @Override
+    public void adjustCpusetCpus(String path, String cpuset, long durationMillis) {
+        File file = new File(path);
+        if (!file.exists()) {
+            return;
+        }
+        String originalCpuset = null;
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            originalCpuset = reader.readLine();
+        } catch (IOException e) {
+            Log.e("adjustCpusetCpus", "Failed to read original cpuset from " + path + ": " + e.getMessage());
+            return;
+        }
+
+        executeAdjustCpusetCpus(path, cpuset);
+
+        String restoreCpuset = originalCpuset;
+        mHandler.postDelayed(() -> executeAdjustCpusetCpus(path, restoreCpuset), durationMillis);
+    }
+
+    private String getTopAppPackageName() {
+        String currentPackage;
+        try {
+            RunningTaskInfo rti = mActivityTaskManager.getTasks(
+                1, false /* filterVisibleRecents */, false /*keepIntentExtra */,
+                INVALID_DISPLAY).get(0);
+            currentPackage = rti.topActivity.getPackageName();
+        } catch (Exception e) {
+            currentPackage = null;
+        }
+        return currentPackage;
+    }
+
+    private boolean isTopAppGame(String packageName) {
+        if (packageName == null) return false;
+        boolean isGame = false;
+        try {
+            ApplicationInfo ai = mContext.getPackageManager().getApplicationInfo(packageName, 0);
+            if(ai != null) {
+                isGame = (ai.category == ApplicationInfo.CATEGORY_GAME) ||
+                        ((ai.flags & ApplicationInfo.FLAG_IS_GAME) ==
+                            ApplicationInfo.FLAG_IS_GAME);
+            }
+        } catch (Exception e) {
+            return false;
+        }
+        return isGame;
+    }
+
+    @Override
+    public void animationBoost(int pid, boolean enabled) {
+        ProcessRecord curProc;
+        synchronized (mPidsSelfLocked) {
+            curProc = mPidsSelfLocked.get(pid);
+        }
+        if (curProc == null) {
+            return;
+        }
+
+        if (enabled) {
+            String topApp = getTopAppPackageName();
+            if (topApp == null) {
+                return;
+            }
+
+            ProcessRecord topAppProc = getProcessRecord(topApp);
+            if (topAppProc == null) {
+                return;
+            }
+
+            int topAppPid = topAppProc.getPid();
+            int group = Process.THREAD_GROUP_DEFAULT;
+            int priority = Process.THREAD_PRIORITY_DEFAULT;
+            try {
+                Process.setProcessGroup(topAppPid, group);
+                Process.setThreadGroupAndCpuset(topAppPid, group);
+                Process.setThreadPriority(topAppPid, priority);
+                setThreadAffinity(topAppPid, 1);
+                mTopAppPid = topAppPid;
+            } catch (Exception e) {
+                Slog.w(TAG, "Failed to demote top-app process: " + e);
+                mTopAppPid = -1;
+                return;
+            }
+        } else {
+            if (mTopAppPid != -1) {
+                try {
+                    Process.setProcessGroup(mTopAppPid, Process.THREAD_GROUP_TOP_APP);
+                    Process.setThreadGroupAndCpuset(mTopAppPid, Process.THREAD_GROUP_TOP_APP);
+                    Process.setThreadPriority(mTopAppPid, Process.THREAD_PRIORITY_TOP_APP_BOOST);
+                    setThreadAffinity(mTopAppPid, 2);
+                } catch (Exception e) {
+                    Slog.w(TAG, "Failed to restore top-app process group: " + e);
+                } finally {
+                    mTopAppPid = -1;
+                }
+            }
+        }
+
+        setFifoPriority(curProc, enabled, 99);
+        mBoostingAnimation = enabled;
+    }
+
+    @Override
+    public void setThreadAffinity(int pid, int affinity) {
+        ProcessRecord curProc;
+        synchronized (mPidsSelfLocked) {
+            curProc = mPidsSelfLocked.get(pid);
+        }
+        if (curProc == null) {
+            return;
+        }
+
+        int threadGroup = (affinity == 0)
+                ? Process.THREAD_GROUP_TOP_APP
+                : Process.THREAD_GROUP_DEFAULT;
+
+        Process.setThreadGroupAndCpuset(pid, threadGroup);
+        Process.setThreadAffinity(pid, affinity);
+        
+        int rTid = curProc.getRenderThreadTid();
+        if (rTid == 0) return;
+        Process.setThreadGroupAndCpuset(rTid, threadGroup);
+        Process.setThreadAffinity(rTid, affinity);
+    }
+
+    @Override
+    public boolean isBoostingAnimation() {
+        return mBoostingAnimation;
+    }
+
+    @Override
+    public void setPerformanceMode(boolean enabled) {
+        if (enabled && isTopAppGame(getTopAppPackageName())) return;
+        if (mLocalPowerManager != null) {
+            // alway reset to retrigger LAUNCH powerhint
+            if (enabled) {
+                mLocalPowerManager.setPowerMode(Mode.LAUNCH, false);
+            }
+            mLocalPowerManager.setPowerMode(Mode.LAUNCH, enabled);
+            mLocalPowerManager.setPowerMode(
+                PowerManagerInternal.MODE_FIXED_PERFORMANCE, enabled);
+        }
+    }
+
+    public class ProcessComparator implements Comparator<ProcessToKill> {
+        @Override
+        public int compare(ProcessToKill p1, ProcessToKill p2) {
+            return Integer.compare(p2.adj, p1.adj);
+        }
+    }
+
+    public static final class ProcessToKill {
+        public int adj;
+        public String name; 
+        public int pid;
+        public int uid;
+        public ProcessRecord record;
+
+        public ProcessToKill(ProcessRecord record) {
+            this.pid = record.getPid();
+            this.uid = record.uid;
+            this.adj = record.getSetAdj();
+            this.name = record.processName;
+            this.record = record;
+        }
+    }
+    
+    private void initTaskProfiles() {
+        String[] bgProfiles = { "ProcessCapacityLow" };
+        String[] bgProcs = { "kswapd", "kcompactd" };
+        setTaskProfilesForProcs(bgProcs, bgProfiles);
+    }
+    
+    public static void setTaskProfilesForProcs(String[] procGroups, String[] profiles) {
+        File procDir = new File("/proc");
+        File[] entries = procDir.listFiles(file -> file.isDirectory() && file.getName().matches("\\d+"));
+        if (entries == null) {
+            Slog.w("setTaskProfilesForProcs", "/proc not accessible or empty.");
+            return;
+        }
+
+        for (File pidDir : entries) {
+            File commFile = new File(pidDir, "comm");
+            String processName = null;
+
+            try (BufferedReader reader = new BufferedReader(new FileReader(commFile))) {
+                processName = reader.readLine();
+            } catch (IOException e) {
+                Slog.w("setTaskProfilesForProcs", "Could not read " + commFile.getPath() + ": " + e);
+                continue;
+            }
+
+            if (processName == null) continue;
+
+            for (String proc : procGroups) {
+                if (processName.contains(proc)) {
+                    try {
+                        int pid = Integer.parseInt(pidDir.getName());
+                        Process.setTaskProfiles(pid, profiles);
+                        Slog.i("setTaskProfilesForProcs", "Applied profiles " + Arrays.toString(profiles) +
+                                " to process " + processName + " (PID " + pid + ")");
+                    } catch (NumberFormatException e) {
+                        Slog.w("setTaskProfilesForProcs", "Invalid PID: " + pidDir.getName());
+                    } catch (Exception e) {
+                        Slog.w("setTaskProfilesForProcs", "Failed to set profiles for PID " + pidDir.getName() + ": " + e);
+                    }
+                    break;
+                }
+            }
+        }
     }
 }